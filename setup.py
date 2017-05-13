6fe194187ebb7e1f05f7052f3d9f9240c828a09e
benmontet <btm@astro.caltech.edu>
2017-05-12 23:46:15 -0500

    small change to setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
-from distutils.core import setup
+from setuptools import setup
+import f3
 
 setup(
-    name=‘f3',
-    version='0.1dev',
-    packages=[‘photometry’,],
-    license=‘MIT’,
-    long_description=open('README.txt').read(),
-    author=‘Ben Montet’,
-    author_email=‘bmontet@uchicago.edu’,
-    url=‘http://github.com/benmontet/f3’,
-    description=‘Photometry for Kepler Full Frame Images’,
+    name='f3',
+    version='1.0',
+    license='MIT',
+    long_description=open('README.rst').read(),
+    author='Ben Montet',
+    author_email='bmontet@uchicago.edu',
+    packages=[
+        'f3',
+    ],
+    url='http://github.com/benmontet/f3',
+    description='Photometry for Kepler Full Frame Images',
+    package_data={'': ['README.rst', 'LICENSE']},
     install_requires=[
-          ‘mahotas’,
+          'mahotas',
       ],
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python 2.7”,
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
     ],
 )
